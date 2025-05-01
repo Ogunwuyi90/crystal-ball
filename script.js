402d3845b39cf8c8f9ead072365e145a58af41de
Ogunwuyi90 <tobiogunwuyi@achievementfirst.org>
2025-05-01 10:32:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+console.log("hello, anyone?");
+
+let answerDiv=document.querySelector(".answer"); 
+let image=document.querySelector(".ball");
+let body=document.querySelector("body");
+let changeSize=document.querySelector(".changeSize"); 
+let changeColor=document.querySelector(".changeColor"); 
+let sayWeather=document.querySelector(".sayWeather");
+let sayCity=document.querySelector(".sayCity");
+let changeText=document.querySelector(".changeText");
+let suggestLunch=document.querySelector(".suggestLunch");
+let sayNumber=document.querySelector(".sayNumber");
+let changePicture=document.querySelector(".changePicture");
+
+sayWeather.onclick=function(){
+    //Write the code for Code Along Part 1 below!
+    
+  };
+
+sayCity.onclick=function(){
+    //Write the code for Code It Solo Part 1 below!
+
+  };
+
+changeSize.onclick=function(){
+    //Write the code for Code Along Part 2 below
+    answerDiv.innerHTML = "";
+};
+
+changeColor.onclick=function(){
+    //Write the code for Code It Solo Part 2 below
+    
+  
+};
+
+changeText.onclick=function(){
+    //Write the code for Code It Solo Part 3 Problem 1 below
+
+  };
+
+suggestLunch.onclick=function(){
+    //Write the code for Code It Solo Part 3 Problem 2 below
+
+  };
+
+sayNumber.onclick=function(){
+ let secretNumber=document.querySelector("input").value; //this is saving the value of whatever the user types in as their secrete number
+//Write the code for Spicy Problem 3 below
+
+  };
+
+changePicture.onclick=function(){
+    //Write the code for Spicy Problem 4 below. Remember to use .src!
+
+  };
+
